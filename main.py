2abe9b9ddd856ddedcd5b31cc58edf46f3983a51
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2018-08-23 16:19:03 +0900

    Download framework directory

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import click
 from generator import YamlGenerator
+import requests, zipfile, io, os
 
 
 @click.command()
 @click.option('--app_name', prompt="Your application name", help='Name of application')
 def execute(app_name):
     """Simple program that generates sample YAML file."""
+    download_framework(app_name)
     YamlGenerator().execute(app_name)
     click.echo('Created output.yml.')
+
+
+def download_framework(app_name: str):
+    branch = 'master'
+    url = 'https://github.com/Innovatube/pipeline-framework/archive/' + branch + '.zip'
+    request = requests.get(url, stream=True)
+    zip = zipfile.ZipFile(io.BytesIO(request.content))
+    zip.extractall()
+    os.rename("pipeline-framework-" + branch, app_name)
+    zip.close()
 
 
 if __name__ == '__main__':
